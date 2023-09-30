15d3669266096d81018e9324597f2b50df25f918
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2023-09-30 15:25:50 -0500

    Punto 1

---
+++ pruebacuenta/CuentaAhorros.java
@@ -0,0 +1,39 @@
+package pruebacuenta;
+
+public class CuentaAhorros extends Cuenta{
+    private boolean activa;
+
+    public CuentaAhorros(float saldo, float tasa) {
+        super(saldo, tasa);
+        if (saldo < 10000){
+            activa = false;}else{
+            activa = true;}
+    }
+
+    public void retirar(float cantidad) {
+        if (activa) {
+            super.retirar(cantidad);}
+    }
+
+    public void consignar(float cantidad) {
+        if (activa) {
+            super.consignar(cantidad); }
+    }
+
+    public void extractoMensual() {
+
+        if (númeroRetiros > 4) {
+            comisiónMensual += (númeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual(); 
+        if ( saldo < 10000 ){
+            activa = false;}
+}
+
+    public void imprimir() {
+        System.out.println("Saldo = $ " + saldo);
+        System.out.println("Comision mensual = $" +comisiónMensual);
+        System.out.println("Numero de transacciones = " +(númeroConsignaciones + númeroRetiros));
+        System.out.println();
+    }
+}
