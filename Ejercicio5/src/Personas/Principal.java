6137034afc2f679737956c25dafb1f89a9055cef
SATVanegas <124936176+SATVanegas@users.noreply.github.com>
2023-10-10 11:24:37 -0500

    Ejercicio 5 Añadido

---
+++ Ejercicio5/src/Personas/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+/**
+ * Esta clase define el punto de ingreso al programa de gestión de personas. Por
+ * lo tanto, cuenta con un método main de acceso al programa.
+ *
+ * @version 1.2/2020
+ */
+public class Principal {
+
+    /**
+     * Método main que sirve de punto de entrada al programa
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        /* Define la ventana 
+principal */
+        miVentanaPrincipal = new VentanaPrincipal();
+        /* Crea la ventana 
+principal */
+        miVentanaPrincipal.setVisible(true);
+        /* Establece la ventana 
+como visible */
+    }
+}
